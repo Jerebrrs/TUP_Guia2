26ffa623a92499538a8012dde24f09b543a019e6
Kevin Jeremias Barrios <kevinbrrs96@gmail.com>
2025-08-18 01:37:37 -0300

    Ejercicio 2 - Clase servicio/persona

---
+++ Ejercicio-2/models/Servicio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2.models
+{
+    internal class Servicio
+    {
+
+
+        public Persona VerPersonaPorDni(int dni)
+        {
+            int idx = -1;
+            int n = 0;
+            while(idx ==-1 && n<)
+        }
+    }
+}
